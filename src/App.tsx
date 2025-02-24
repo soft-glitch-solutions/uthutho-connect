8e303d24ac59a6a783e4e3ece4fbda60736bd704
shaqeelless4 <shaqeelless4@gmail.com>
2025-02-24 13:43:49 +0000

    update changes here.

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import { Onboarding } from "./components/Onboarding";
 import { MainLayout } from "./components/layout/MainLayout";
 import Auth from "./pages/Auth";
-import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 import Hubs from "./pages/Hubs";
 import TransportRoutes from "./pages/Routes";
 import HubRequest from "./pages/HubRequest";
 import RouteRequest from "./pages/RouteRequest";
 import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import Stop from "./pages/Profile";
 
 const queryClient = new QueryClient();
 
@@ -25,11 +27,13 @@ const App = () => (
         <RouterRoutes>
           <Route path="/" element={<Onboarding />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/dashboard" element={<MainLayout><Dashboard /></MainLayout>} />
+          <Route path="/home" element={<MainLayout><Home /></MainLayout>} />
           <Route path="/hubs" element={<MainLayout><Hubs /></MainLayout>} />
           <Route path="/routes" element={<MainLayout><TransportRoutes /></MainLayout>} />
           <Route path="/hub-request" element={<MainLayout><HubRequest /></MainLayout>} />
           <Route path="/route-request" element={<MainLayout><RouteRequest /></MainLayout>} />
+          <Route path="/stops" element={<MainLayout><Stop /></MainLayout>} />
+          <Route path="/profile" element={<MainLayout><Profile /></MainLayout>} />
           <Route path="*" element={<NotFound />} />
         </RouterRoutes>
       </BrowserRouter>
